a7e224858a2a89ece3942f03b6cc5bb44c2116dd
percy <p3r.valderrama@gmail.com>
2024-09-10 03:33:33 -0500

    feat-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import PresentacionConvercion from "@/components/presentacion-convercion";
 
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      cont
+    <div className="">
+      <PresentacionConvercion />
     </div>
   );
 }
